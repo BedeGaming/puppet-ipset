88e6566dad8df59f9acb2bf08aa0dc98b825fe0a
Sava Varadzhakov <varadjakov@gmail.com>
2018-04-26 17:34:45 +0300

    remove internal methods

--- lib/puppet/parser/functions/get_ipsets_from_consul.rb
+++ lib/puppet/parser/functions/get_ipsets_from_consul.rb
@@ -5,50 +5,38 @@ module Puppet::Parser::Functions
         require 'json'
         require "base64"
         
-        def get_consul_values_as_hash (url)
-        
-            uri = URI.parse(url)
-            response = Net::HTTP.get_response(uri)
-            
-            responseBodyParsed = ""
-            if response.code === "200"
-                responseBodyParsed = JSON.parse(response.body)    
-            else
-                raise response.message
-            end
-            
-            # responses from consul API come as base64 encoded
-            responseBodyDecoded = Base64.decode64(responseBodyParsed.first["Value"])
-            
-            # return parsed version of the config as a hash
-            JSON.parse(responseBodyDecoded)
-        end
-        
-        def remove_magic_strings(foo)
-            
-            fooReplaced = {}
-            
-            foo.each do |k, v| 
-                # remove "ipsets." magic words from keys
-                kReplaced = k.gsub("ipsets.", "")
-                
-                # replace commas with newlines in the values
-                vReplaced = v.gsub(", ", "\n")
-                
-                fooReplaced[kReplaced] = vReplaced
-            end
-        
-            fooReplaced
-        end
-        
         # get url from args
         url = args[0]
+        
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        
+        responseBodyParsed = ""
+        if response.code === "200"
+            responseBodyParsed = JSON.parse(response.body)    
+        else
+            raise response.message
+        end
+        
+        # responses from consul API come as base64 encoded
+        responseBodyDecoded = Base64.decode64(responseBodyParsed.first["Value"])
+        
+        # get parsed version of the config as a hash
+        ipsets = JSON.parse(responseBodyDecoded)
+        
+        fooReplaced = {}
             
-        # get ipsets from consul
-        ipsets = get_consul_values_as_hash(url)
-        
-        # remove magic strings needed, so ipsets fit in Conan
-        remove_magic_strings(ipsets)
-        
+        foo.each do |k, v| 
+            # remove "ipsets." magic words from keys
+            kReplaced = k.gsub("ipsets.", "")
+            
+            # replace commas with newlines in the values
+            vReplaced = v.gsub(", ", "\n")
+            
+            fooReplaced[kReplaced] = vReplaced
+        end
+    
+        fooReplaced
+
     end
  end
