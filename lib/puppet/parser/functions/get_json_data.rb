0f2af19cb73306f7443bd965841d3e582d96a3b6
Sava Varadzhakov <varadjakov@gmail.com>
2018-04-26 14:50:09 +0300

    update function

--- lib/puppet/parser/functions/get_json_data.rb
+++
@@ -1,7 +0,0 @@
-module Puppet::Parser::Functions
-    newfunction(:get_json_data) do |args|
-      url = args[0]
-      $response = Net::HTTP.get_response(URI.parse(url))
-      JSON.parse($response)
-    end
-  end
