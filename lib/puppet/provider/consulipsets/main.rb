3b6f91929b3a3aac457c3023d42f722d4adf83f3
Sava Varadzhakov <varadjakov@gmail.com>
2018-06-05 15:27:22 +0300

    Try with providers

---
+++ lib/puppet/provider/consulipsets/main.rb
@@ -0,0 +1,55 @@
+Puppet::Type.type(:consulipsets).provide(:main) do
+    
+    require 'net/http'
+    require 'json'
+    require "base64"
+    
+    def get_consul_ipsets(url)
+        
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        
+        responseBodyParsed = ""
+        if response.code === "200"
+            responseBodyParsed = JSON.parse(response.body)    
+        else
+            raise response.message
+        end
+        
+        # responses from consul API come as base64 encoded
+        responseBodyDecoded = Base64.decode64(responseBodyParsed.first["Value"])
+        
+        # get parsed version of the config as a hash
+        ipsets = JSON.parse(responseBodyDecoded)
+        
+        ipsetsGroupedByRuleAndPriority = {}
+        
+        ipsets.each do |ipset_name, ipset_value| 
+    
+            # remove "ipsets." magic words from keys
+            nameReplaced = ipset_name.gsub("ipsets.", "")
+                   
+            ipset_value.each do |ip, details|  
+    
+                # construct the ipset name (e.g savagaming_accept or savagaming_drop)
+                ipset_name = nameReplaced + "_" + details["Rule"] + "_" + details["Priority"]
+    
+                unless ipsetsGroupedByRuleAndPriority[ipset_name]
+                    ipsetsGroupedByRuleAndPriority[ipset_name] = []
+                end
+    
+                ipsetsGroupedByRuleAndPriority[ipset_name] << ip
+    
+            end
+    
+        end
+    
+        ipsetsGroupedByRuleAndPriority
+    
+    end
+    
+    def consul_ipsets
+        get_consul_ipsets(resource[:consul_ipsets_url])
+    end
+    
+  end
---
+++ lib/puppet/type/consulipsets.rb
@@ -0,0 +1,39 @@
+Puppet::Type.newtype(:consulipsets) do
+    desc "Puppet type that retrieves ipsets from consul and applies them"
+  
+    newparam(:consul_ipsets_url) do
+        desc "Url for ipsets retrieval from Consul"
+        defaultto "http://localhost:8500/v1/kv/config/bundle/ipsets"
+    end
+
+    newproperty(:consul_ipsets) do
+        desc "The ipsets themselves"
+    end
+
+    # newparam(:consul_address) do
+    #     desc "Address of the Consul agent"
+    #     defaultto "localhost"
+    # end
+
+    # newparam(:consul_protocol) do
+    #     desc "Protocol to contact Consul agent API"
+    #     newvalues(:http, :https)
+    #     defaultto "http"
+    # end
+  
+    # newparam(:consul_port) do
+    #     desc "Port Consul agent runs on"
+    #     newvalues(/^\d+$/)
+    #     defaultto "8500"
+    # end
+
+    # newparam(:ipsets_key) do
+    #     desc "Path to the ipsets key in Consul KV store"
+    #     defaultto "config/bundle/ipsets"
+    # end
+
+    # newparam(:consul_api_version) do
+    #     desc "Path to the ipsets key in Consul KV store"
+    #     defaultto "v1"
+    # end
+end
